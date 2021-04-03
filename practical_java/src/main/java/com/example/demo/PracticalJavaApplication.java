f6b08d37ae9dddc907dfc8693d9ef1d7f65df485
veeraswamy <veeraswamyv.19@gmail.com>
2021-04-03 14:17:46 +0530

    commit the java file into github

---
+++ practical_java/src/main/java/com/example/demo/PracticalJavaApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalJavaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PracticalJavaApplication.class, args);
+		System.out.println("hello github");
+	}
+
+}
