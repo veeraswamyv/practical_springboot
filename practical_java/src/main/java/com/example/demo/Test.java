25bc853c384030cdbc9c542f3561a4c1b2281563
veeraswamy <veeraswamyv.19@gmail.com>
2021-04-04 10:11:49 +0530

    comitting test class to dev branch

---
+++ practical_java/src/main/java/com/example/demo/Test.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Test {
+	public static void main(String[] args) {
+		String str = "hello";
+		if (str != null) {
+			System.out.println("feeling hungry");
+		}
+
+	}
+}
